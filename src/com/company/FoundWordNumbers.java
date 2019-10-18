673e8505a7fd3bf45aa29b98f13896eef4f97310
georgiev84 <combytep@gmail.com>
2019-10-18 16:24:11 +0300

    s

--- src/com/company/FoundWordNumbers.java
+++ src/com/company/FoundWordNumbers.java
@@ -10,18 +10,32 @@ public class FoundWordNumbers {
         String input = "John Mike John Mike Donald Ivy John Joe John Donald";
         String[] names = input.split("\\s+");
 
-        HashMap<String, Integer> values = new HashMap<String, Integer>();
-        for (String name : names){
+//        HashMap<String, Integer> values = new HashMap<String, Integer>();
+//        for (String name : names){
+//
+//            int count=0;
+//            for(String doubledName : names){
+//                if(name.equals(doubledName)){
+//                    count++;
+//                }
+//            }
+//            values.put(name,count);
+//
+//        }
 
-            int count=0;
-            for(String doubledName : names){
-                if(name.equals(doubledName)){
-                    count++;
-                }
-            }
-            values.put(name,count);
+           HashMap<String, Integer> values = new HashMap<>();
+           for(int i=0; i<names.length; i++){
+               if(values.keySet().contains(names[i])){
 
-        }
+                   int count = values.get(names[i]);
+                   values.put(names[i], count+1);
+
+               } else {
+                   values.put(names[i], 1);
+               }
+           }
+
+
 
            for(Map.Entry<String, Integer> a : values.entrySet()){
                System.out.println(a);
--- src/com/company/ReverseString.java
+++ src/com/company/ReverseString.java
@@ -6,12 +6,18 @@ public class ReverseString {
         String[] arrStr = str.split("");
         String[] reverse = new String[arrStr.length];
 
-        for(int i=0; i<=arrStr.length/2; i++){
-            String last = arrStr[arrStr.length-1-i];
-            String first = arrStr[i];
+//        for(int i=0; i<=arrStr.length/2; i++){
+//            String last = arrStr[arrStr.length-1-i];
+//            String first = arrStr[i];
+//
+//            reverse[i]=last;
+//            reverse[arrStr.length-1-i] = first;
+//        }
 
-            reverse[i]=last;
-            reverse[arrStr.length-1-i] = first;
+        int count=0;
+        for(int i=arrStr.length-1; i>=0; i--){
+            reverse[count] = arrStr[i];
+            count++;
         }
 
         for(String a : reverse){
